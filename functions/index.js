2ad75a5f3e94cbe9d20aeaaf7698b67fb58d9a25
Stephanie Sanchez <stephk006@gmail.com>
2022-06-07 21:35:44 -0400

    establish connection to database

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,14 @@
-const functions = require("firebase-functions");
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/test", (req, res) => {
+  req.send("🔥 This is working");
+});
+
+app.get("/");
+app.get("/");
